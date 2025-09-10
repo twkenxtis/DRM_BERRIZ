f829427ecfe769c491472ab0fdbe814207f544ea
omenbibi <86098527+twkenxtis@users.noreply.github.com>
2025-09-10 13:27:35 +0800

    Add files via upload

--- unit/berriz_drm.py
+++ unit/berriz_drm.py
@@ -36,12 +36,14 @@ class Key_handle:
     def msprpro(self):
         if self._msprpro is None and self.raw_mpd:
             self._msprpro = GetMPD_prd.parse_pssh(self.raw_mpd)
+            self._msprpro[:] = list(set(self._msprpro))
         return self._msprpro
 
     @property
     def wv_pssh(self):
         if self._wv_pssh is None and self.raw_mpd:
             self._wv_pssh = GetMPD_wv.parse_pssh(self.raw_mpd)
+            self._wv_pssh[:] = list(set(self._wv_pssh))
         return self._wv_pssh
 
     async def send_drm(self):
@@ -51,10 +53,9 @@ class Key_handle:
         
         if hasattr(self.playback_info, "duration"):
             if self.playback_info.is_drm:
-                wv_pssh_value, msprpro_value = await self.search_keys()
+                key = await self.search_keys()
 
-                if msprpro_value is not None:
-                    key = msprpro_value
+                if key is not None:
                     return key, self.media_id
                 key = await self.request_keys()
                 return list(key), self.media_id
@@ -74,50 +75,62 @@ class Key_handle:
             p = self._wv_pssh
         return p
 
-    async def save_key(self, key):
+    async def save_key(self, key: str) -> None:
         vault = SQLiteKeyVault()
-        if key is None:
+        if not key:
             logger.error("Key is None. Cannot save to vault.")
             return
 
-        await asyncio.gather(
-            vault.store_single(self.wv_pssh, key, drm_type="wv"),
-            vault.store_single(self.msprpro, key, drm_type="mspr")
-        )
-
-        for k in [self.wv_pssh, self.msprpro]:
-            drm_type = 'wv' if len(k) > 76 else 'mspr'
-            if vault.contains(k):
-                logger.info(f"{Color.fg('iceberg')}SUCCESS save key to local vault:{Color.reset()} "
-                            f"{Color.fg('gold')}{key}{Color.reset()} - {Color.fg('ruby')}{drm_type}{Color.reset()}"
-                            )
+        async def _store_and_log(pssh: str, drm: str):
+            await vault.store_single(pssh, key, drm)
+            exists = vault.contains(pssh)
+            tag, reset = Color.fg("iceberg"), Color.reset()
+            if exists:
+                logger.info(
+                    f"{tag}SUCCESS save key to local vault:{reset} "
+                    f"{Color.fg('gold')}{key}{reset} - "
+                    f"{Color.fg('ruby')}{drm}{reset}"
+                )
             else:
-                logger.error(f"Key verification FAILED for: {k}")
+                logger.error(f"Key verification FAILED for: {pssh}")
+
+        jobs = []
+        if self.wv_pssh:
+            for wv_pssh in self.wv_pssh:
+                jobs.append(_store_and_log(wv_pssh, "wv"))
+        if self.msprpro:
+            for mspr_pssh in self.msprpro:
+                jobs.append(_store_and_log(mspr_pssh, "mspr"))
+        if jobs:
+            await asyncio.gather(*jobs)
 
     async def search_keys(self):
         vault = SQLiteKeyVault()
-        results = await asyncio.gather(
-            vault.retrieve(self.wv_pssh),
-            vault.retrieve(self.msprpro)
-        )
-        wv_pssh_value, msprpro_value = results
-        if msprpro_value or wv_pssh_value is not None:
-            logger.info(f"{Color.fg('mint')}Use local key vault keys:{Color.reset()} {Color.fg('ruby')}{msprpro_value}{Color.reset()}")
-            return wv_pssh_value, msprpro_value
-        return (None, None)
+        if self.wv_pssh:
+            for all_pssh in self.wv_pssh:
+                wv = await vault.retrieve(all_pssh)
+        if self.msprpro:
+            for all_pssh in self.msprpro:
+                ms_pr = await vault.retrieve(all_pssh)
+            key = ms_pr or wv 
+            if key is not None:
+                logger.info(f"{Color.fg('mint')}Use local key vault keys:{Color.reset()} {Color.fg('ruby')}{key}{Color.reset()}")
+                return key
+        return (None)
 
     async def request_keys(self):
         """wv mspr cdrm watora"""
         request_pssh = await self.drm_choese()
-        keys = await get_clear_key(request_pssh, self.assertion, self.drm_type)
-        
-        if keys:
-            for key in keys:
-                await self.save_key(key)
-            return keys
-        else:
-            logger.error("No keys received")
-            return None
+        for pssh in request_pssh:
+            keys = await get_clear_key(pssh, self.assertion, self.drm_type)
+            
+            if keys:
+                for key in keys:
+                    await self.save_key(key)
+                return keys
+            else:
+                logger.error("No keys received")
+                return None
 
 
 async def start_download(public_info, key, raw_mpd, dash_playback_url, hls_playback_url, raw_hls):
@@ -194,11 +207,9 @@ class BerrizProcessor:
             raw_hls = await rebuild_master_playlist(response_hls, playback_info.hls_playback_url)
         if playback_info.is_drm is True:
             key_handler = Key_handle(playback_info, self.media_id, raw_mpd)
-            logger.info(f"{Color.fg('orange')}{key_handler.wv_pssh}{Color.reset()}")
-            logger.info(f"{Color.fg('yellow')}{key_handler.msprpro}{Color.reset()}")
-            
-            k = await key_handler.send_drm()
-            key, media_id_from_drm = k
+            await self.print_drm_info(key_handler)
+            pk = await key_handler.send_drm()
+            key, media_id_from_drm = pk
         elif playback_info.is_drm is False:
             key = None
         else:
@@ -215,6 +226,28 @@ class BerrizProcessor:
         )
         self._tasks.append(task)
 
+    async def print_drm_info(self, key_handler):
+        k = key_handler.wv_pssh
+        p = key_handler.msprpro
+        if k is not None and isinstance(k, list):
+            k_print ='\n'.join(k)
+            logger.info(f"{Color.fg('iron')}PSSH: "
+                        f"{Color.fg('orange')}{k_print}{Color.reset()}"
+                        )
+            logger.info(
+                f"{Color.fg('light_gray')}encryption support:{Color.reset()} "
+                f"{Color.fg('bright_cyan')}Widevine{Color.reset()}"
+            )
+        if p is not None and isinstance(p, list):
+            p_print = '\n'.join(p)
+            logger.info(f"{Color.fg('iron')}PSSH: "
+                f"{Color.fg('yellow')}{p_print}{Color.reset()}"
+                )
+            logger.info(
+                f"{Color.fg('light_gray')}encryption support:{Color.reset()} "
+                f"{Color.fg('bright_cyan')}PlayReady{Color.reset()}"
+            )
+        
     @cache
     async def execute_downloads(self):
         if not self._tasks:
