5fea210093541fc712572d7015bbf47c9e5af780
omenbibi <86098527+twkenxtis@users.noreply.github.com>
2025-09-12 01:48:43 +0800

    Add files via upload

--- mystate/parse_my.py
+++ mystate/parse_my.py
@@ -1,10 +1,8 @@
 import asyncio
 
-from cookies.cookies import Berriz_cookie
 from static.color import Color
 from unit.handle_log import setup_logging
-from unit.http.request_berriz_api import My
-
+from unit.http.request_berriz_api import My, BerrizAPIClient
 
 
 logger = setup_logging('parse_my', 'ruby')
@@ -12,9 +10,7 @@ logger = setup_logging('parse_my', 'ruby')
 
 async def request_my():
     # 檢查 cookie，若無則直接返回
-    if not Berriz_cookie()._cookies:
-        return
-
+    await retry()
     try:
         data, locat, notif, me_data = await asyncio.gather(
             My().fetch_my(),
@@ -45,14 +41,38 @@ async def request_my():
         logger.info(
             f"{Color.fg('royal_blue')}Login to: {Color.fg('crimson')}{my_id}{Color.reset()}"
             f" {Color.fg('royal_blue')}Mail: {Color.fg('periwinkle')}{my_email}{Color.reset()}"
-            f" {Color.fg('royal_blue')}-> {Color.fg('amber')}{location}{Color.reset()}"
-            f" {Color.fg('royal_blue')}-> {Color.fg('khaki')}\n[{memberKey} {Color.fg('lavender')}{email} {Color.fg('lemon')}{passwordRegistered} "
-            f"{Color.fg('chocolate')}{passwordMismatchCount}"
-            f" {status} {Color.fg('periwinkle')}{createdAt} {Color.fg('indigo')}{updatedAt}]{Color.reset()}"
+            f" {Color.fg('royal_blue')}➤  {Color.fg('amber')}{location}{Color.reset()}"
+            f" {Color.fg('royal_blue')}\n[memberKey: {Color.fg('khaki')}{memberKey} {Color.fg('lavender')}{email} "
+            f"{Color.fg('royal_blue')}passwordRegistered: {Color.fg('lemon')}{passwordRegistered} "
+            f"{Color.fg('royal_blue')}passwordMismatchCount: {Color.fg('chocolate')}{passwordMismatchCount} "
+            f"{Color.fg('royal_blue')}status: {Color.fg('daffodil')}{status} \n{Color.fg('royal_blue')}createdAt: "
+            f"{Color.fg('periwinkle')}{createdAt} {Color.fg('royal_blue')}updatedAt: "
+            f"{Color.fg('indigo')}{updatedAt}]{Color.reset()}"
         )
 
         if keys:
             logger.info(f"{Color.fg('gray')}My joined communities: {Color.fg('pink')}{' | '.join(keys)}")
-            
+    except AttributeError as e:
+        if "NoneType" in str(e):
+            logger.error(f"Check API response maybe is 401? - 'Nonetype'")
     except Exception as e:
         logger.error(f"An unexpected error occurred during API requests: {e}")
+        
+async def retry():
+        retry_count = 0
+        max_retries = 6
+        while retry_count < max_retries:
+            try:
+                is_cookie_valid = await BerrizAPIClient().cookie()
+                if is_cookie_valid:
+                    break
+                else:
+                    retry_count += 1
+                    await asyncio.sleep(0.25)
+
+            except Exception as e:
+                retry_count += 2
+                await asyncio.sleep(1)
+
+        if retry_count == max_retries:
+            return
