f92def30dad30fcebba21d4d09f2142253e4ebea
omenbibi <86098527+twkenxtis@users.noreply.github.com>
2025-09-12 03:06:13 +0800

    Add files via upload

--- unit/main_process.py
+++ unit/main_process.py
@@ -1,19 +1,19 @@
-import aiofiles
 import asyncio
-import orjson
+from functools import lru_cache
 import os
 import sys
-from functools import lru_cache
-
 from typing import List
 
-from cookies.cookies import Berriz_cookie
-from unit.berriz_drm import BerrizProcessor
-from unit.image.image import run_image_dl
+import aiofiles
+import orjson
+
 from lib.media_queue import MediaQueue
 from lock.donwnload_lock import UUIDSetStore
 from static.color import Color
+from unit.berriz_drm import BerrizProcessor
 from unit.handle_log import setup_logging
+from unit.http.request_berriz_api import BerrizAPIClient
+from unit.image.image import run_image_dl
 from unit.parameter import paramstore
 
 
@@ -75,7 +75,7 @@ class MediaProcessor:
 
     async def _process_vod_items(self, media_id: str, media_type) -> None:
         """Process VOD items using BerrizProcessor."""
-        if len(Berriz_cookie()._cookies) == 0:
+        if await BerrizAPIClient().cookie() != {}:
             logger.warning(f"{Color.fg('light_gray')}Cookies are required to download {Color.bg('crimson')}videos{Color.reset()}")
             logger.info(f"{Color.fg('gold')}Skip {media_id} video download{Color.reset()}")
             return
