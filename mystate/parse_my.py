5849d8506de9ba57fab92c728fa2ac748805ed1f
omenbibi <86098527+twkenxtis@users.noreply.github.com>
2025-09-15 17:19:49 +0800

    Add files via upload

--- mystate/parse_my.py
+++ mystate/parse_my.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from cookies.cookies import Berriz_cookie
+
 from static.color import Color
 from lib.lock_cookie import cookie_session, Lock_Cookie
 from unit.handle_log import setup_logging
@@ -12,22 +12,24 @@ logger = setup_logging('parse_my', 'ruby')
 
 async def request_my():
     if cookie_session in ({}, None):
-        return
+        await Lock_Cookie.cookie_session()
+
     try:
-        data, locat, notif, me_data = await asyncio.gather(
+        data, locat, notif, my_data, me = await asyncio.gather(
             My().fetch_my(),
             My().fetch_location(),
             My().notifications(),
-            My().fetch_me()
+            My().fetch_me(),
+            My().get_me_info()
         )
-        if all([data, locat, notif, me_data]) is not None:
+        if all([data, locat, notif, my_data, me]) is not None:
             # 提取資料並使用 .get() 方法，以避免 KeyError
             my_id = data.get('data', {}).get('memberInfo', {}).get('memberKey')
             my_email = data.get('data', {}).get('memberInfo', {}).get('memberEmail')
             
             location = locat.get('data', {}).get('countryCode')
 
-            me_info = me_data.get('data', {})
+            me_info = my_data.get('data', {})
             memberKey = me_info.get('memberKey')
             email = me_info.get('email')
             passwordRegistered = me_info.get('passwordRegistered')
@@ -35,6 +37,11 @@ async def request_my():
             status = me_info.get('status')
             createdAt = me_info.get('createdAt')
             updatedAt = me_info.get('updatedAt')
+            me_info = me.get('data', {})
+            email2 = me_info.get('email')
+            contactEmail = me_info.get('contactEmail')
+            country = me_info.get('country')
+            phoneNumber= me_info.get('phoneNumber')
 
             # 提取社羣金鑰
             join_community = notif.get('data', {}).get('contents', [])
@@ -44,12 +51,16 @@ async def request_my():
                 f"{Color.fg('royal_blue')}Login to: {Color.fg('crimson')}{my_id}{Color.reset()}"
                 f" {Color.fg('royal_blue')}Mail: {Color.fg('periwinkle')}{my_email}{Color.reset()}"
                 f" {Color.fg('royal_blue')}➤  {Color.fg('amber')}{location}{Color.reset()}"
-                f" {Color.fg('royal_blue')}\n[memberKey: {Color.fg('khaki')}{memberKey} {Color.fg('lavender')}{email} "
+                f" {Color.fg('royal_blue')}\nmemberKey: {Color.fg('khaki')}{memberKey} {Color.fg('lavender')}{email} "
                 f"{Color.fg('royal_blue')}passwordRegistered: {Color.fg('lemon')}{passwordRegistered} "
                 f"{Color.fg('royal_blue')}passwordMismatchCount: {Color.fg('chocolate')}{passwordMismatchCount} "
                 f"{Color.fg('royal_blue')}status: {Color.fg('daffodil')}{status} \n{Color.fg('royal_blue')}createdAt: "
                 f"{Color.fg('periwinkle')}{createdAt} {Color.fg('royal_blue')}updatedAt: "
-                f"{Color.fg('indigo')}{updatedAt}]{Color.reset()}"
+                f"{Color.fg('indigo')}{updatedAt}{Color.reset()}"
+                f"{Color.fg('flamingo_pink')} email: {Color.fg('dark_red')}{email2} \n{Color.fg('flamingo_pink')}contactEmail:"
+                f"{Color.fg('light_cyan')} {contactEmail} {Color.reset()}"
+                f"{Color.fg('flamingo_pink')}country: {Color.fg('gold')}{country} "
+                f"{Color.fg('flamingo_pink')}phoneNumber: {Color.fg('sunflower')} {phoneNumber} "
             )
 
             if keys:
