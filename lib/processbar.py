a626b0479175da9be221d84269d5cef13faebf35
omenbibi <86098527+twkenxtis@users.noreply.github.com>
2025-10-06 00:35:22 +0800

    Add files via upload

---
+++ lib/processbar.py
@@ -0,0 +1,42 @@
+from rich.console import Console
+from time import time
+
+class ProgressBar:
+    def __init__(self, total: int, prefix: str = "", length: int = 65):
+        self.total = total
+        self.prefix = prefix
+        self.length = length
+        self.current = 0
+        self.console = Console()
+        self.start_time = time()
+
+    def _format_time(self, seconds: float) -> str:
+        m, s = divmod(int(seconds), 60)
+        return f"{m:02d}:{s:02d}"
+
+    def update(self, progress: int = None):
+        if progress is not None:
+            self.current = progress
+        else:
+            self.current += 1
+
+        percent = self.current / self.total
+        filled_length = int(self.length * percent)
+        bar = (
+            "[magenta]" + "_" * filled_length + "[/magenta]"
+            + "[grey37]" + "/" * (self.length - filled_length) + "[/grey37]"
+        )
+        elapsed = time() - self.start_time
+        elapsed_str = self._format_time(elapsed)
+
+        text = (
+            f"[gray]{self.prefix:6}[/gray]: {bar} "
+            f"{percent*100:6.2f} % | {self.current}/{self.total} "
+            f" [red]{elapsed_str}[/red]"
+        )
+
+        self.console.print(text, end="\r")
+
+    def finish(self):
+        self.update(self.total)
+        self.console.print("")
